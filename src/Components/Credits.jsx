f2a4fd23df8fd75aa054b5b4e3c221a8678fa967
Bruno Ogbennaya <bruno.ogbennaya@gmail.com>
2023-06-25 23:23:48 -0400

    comments

--- src/Components/Credits.jsx
+++ src/Components/Credits.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import CreditsDisplay from './CreditsDisplay';
 
 function Credits() {
@@ -7,42 +6,18 @@ function Credits() {
   const [creditDescription, setCreditDescription] = useState('');
   const [creditAmount, setCreditAmount] = useState('');
 
-  useEffect(() => {
-    fetchCredits();
-  }, []);
+  const addCredit = () => {
+    const newCredit = {
+      description: creditDescription,
+      amount: Number(creditAmount),
+      date: new Date().toISOString().split('T')[0],
+    };
 
-  const fetchCredits = async () => {
-    try {
-      const response = await axios.get('https://bank-of-react-b745wfs0u-ajlapid718.vercel.app/credits');
-      setCredits(response.data);
-      console.log('Fetched credits:', response.data.credits);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const updatedCredits = [...credits, newCredit];
+    setCredits(updatedCredits);
 
-  const addCredit = async () => {
-    try {
-      const newCredit = {
-        description: creditDescription,
-        amount: Number(creditAmount),
-        date: new Date().toISOString().split('/')[0],
-      };
-
-      await axios.post(
-        'https://bank-of-react-b745wfs0u-ajlapid718.vercel.app/credits',
-        newCredit
-      );
-      
-      console.log('Credit added:', newCredit);
-
-      setCreditDescription('');
-      setCreditAmount('');
-
-      fetchCredits(); // Refresh credits after adding a new one
-    } catch (error) {
-      console.error(error);
-    }
+    setCreditDescription('');
+    setCreditAmount('');
   };
 
   const handleForm = (event) => {
@@ -75,11 +50,10 @@ function Credits() {
         </label>
 
         <button type="submit">Add Credit</button>
-
       </form>
 
       {Array.isArray(credits) && credits.length > 0 ? (
-        <CreditsDisplay credits={credits} /> 
+        <CreditsDisplay credits={credits} />
       ) : (
         <p>No credits found.</p>
       )}
@@ -88,6 +62,107 @@ function Credits() {
 }
 
 export default Credits;
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CreditsDisplay from './CreditsDisplay';
+
+function Credits() {
+  const [credits, setCredits] = useState([]);
+  const [creditDescription, setCreditDescription] = useState('');
+  const [creditAmount, setCreditAmount] = useState('');
+
+  useEffect(() => {
+    fetchCredits();
+  }, []);
+
+  const fetchCredits = async () => {
+    try {   // Making a GET request to fetch credits data from the API
+      const response = await axios.get('https://bank-of-react-b745wfs0u-ajlapid718.vercel.app/credits');
+      setCredits(response.data);
+      console.log('Fetched credits:', response.data.credits);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addCredit = async () => {
+    try {
+      const newCredit = {
+        description: creditDescription, // Assigning the value of 'creditDescription' to the 'description' property of the new credit object
+        amount: Number(creditAmount), // Converting the 'creditAmount' string to a number and assigning it to the 'amount' property of the new credit object
+        date: new Date().toISOString().split('T')[0], // Creating a new Date object, converting it to an ISO string, and extracting only the date part
+      };
+
+      
+      console.log('Credit added:', newCredit); // Logging the newly added credit object to the console to ensure it was read in correctly
+
+      setCreditDescription(''); // Resetting the 'creditDescription' state to an empty string
+      setCreditAmount(''); // Resetting the 'creditAmount' state to an empty string
+
+      fetchCredits(); // Refresh credits after adding a new one
+    } catch (error) {
+      console.error(error); //Logs any error that occurred while adding a new credit
+    }
+  };
+
+  const handleForm = (event) => {
+    event.preventDefault(); // Preventing the default form submission behavior
+
+    addCredit();  // Calling the addCredit function to add a new credit
+  };
+
+  return (
+    <div>
+      <h2>Credits</h2>
+
+      <form onSubmit={handleForm}>
+        <label>
+          Description:
+          <input
+            type="text"
+            value={creditDescription}
+            onChange={(e) => setCreditDescription(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Amount:
+          <input
+            type="number"
+            value={creditAmount}
+            onChange={(e) => setCreditAmount(e.target.value)}
+          />
+        </label>
+
+        <button type="submit">Add Credit</button>
+
+      </form>
+
+      {Array.isArray(credits) && credits.length > 0 ? (
+        <CreditsDisplay credits={credits} /> // Rendering the CreditsDisplay component with the 'credits' data as a prop if the 'credits' array is not empty
+      ) : (
+        <p>No credits found.</p> // Rendering a paragraph element with the text "No credits found." if the 'credits' array is empty
+      )}
+    </div>
+  );
+}
+
+export default Credits; // Exporting the Credits component as the default export
+
 
 
 
--- src/Components/Debits.jsx
+++ src/Components/Debits.jsx
@@ -1,4 +1,70 @@
-import React, { useState, useEffect } from 'react';
+  
+import React, { useState } from 'react';
+import DebitsDisplay from './DebitsDisplay'; // Importing the DebitsDisplay component
+
+function Debits() {
+  const [debits, setDebits] = useState([]); // State variable to store the debits array
+  const [debitDescription, setDebitDescription] = useState(''); // State variable to store the debit description
+  const [debitAmount, setDebitAmount] = useState(''); // State variable to store the debit amount
+
+  const addDebit = () => {
+    const newDebit = {
+      description: debitDescription, // Assigning the value of 'debitDescription' to the 'description' property of the new debit object
+      amount: Number(debitAmount), // Converting the 'debitAmount' string to a number and assigning it to the 'amount' property of the new debit object
+      date: new Date().toISOString().split('T')[0], // Creating a new Date object, converting it to an ISO string, and extracting only the date part
+    };
+
+    const updatedDebits = [...debits, newDebit]; // Creating a new array by spreading the existing 'debits' array and adding the new debit object
+    setDebits(updatedDebits); // Updating the 'debits' state with the updated array of debits
+
+    setDebitDescription(''); // Resetting the 'debitDescription' state to an empty string
+    setDebitAmount(''); // Resetting the 'debitAmount' state to an empty string
+  };
+
+  return (
+    <div>
+      <h2>Debits</h2>
+
+      <label>
+        Description:
+        <input
+          type="text"
+          value={debitDescription} // Binding the value of the input field to the 'debitDescription' state
+          onChange={(e) => setDebitDescription(e.target.value)} // Updating the 'debitDescription' state with the new value entered in the input field
+        />
+      </label>
+
+      <label>
+        Amount:
+        <input
+          type="number"
+          value={debitAmount} // Binding the value of the input field to the 'debitAmount' state
+          onChange={(e) => setDebitAmount(e.target.value)} // Updating the 'debitAmount' state with the new value entered in the input field
+        />
+      </label>
+
+      <button onClick={addDebit}>Add Debit</button> 
+
+      {Array.isArray(debits) ? (
+        <DebitsDisplay debits={debits} /> // Rendering the DebitsDisplay component with the 'debits' data as a prop if the 'debits' array is not empty
+        ) : (
+        <p>No debits found.</p> // Rendering a paragraph element with the text "No debits found." if the 'debits' array is empty
+      )}
+    </div>
+  );
+}
+
+export default Debits; // Exporting the Debits component as the default export
+
+  
+  
+  
+  
+  
+  
+  
+  
+/*  import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DebitsDisplay from './DebitsDisplay';
 
@@ -17,6 +83,7 @@ function Debits() {
         `https://bank-of-react-b745wfs0u-ajlapid718.vercel.app/debits`
       );
       setDebits(response.data);
+      console.log('Fetched debits:', response.data.debits);
     } catch (error) {
       console.error(error);
     }
@@ -30,10 +97,7 @@ function Debits() {
         date: new Date().toISOString().split('T')[0],
       };
 
-      await axios.post(
-        `https://bank-of-react-b745wfs0u-ajlapid718.vercel.app/debits`,
-        newDebit
-      );
+      console.log('Add debits:', newDebit);
 
       setDebitDescription('');
       setDebitAmount('');
@@ -77,4 +141,4 @@ function Debits() {
   );
 }
 
-export default Debits;
+export default Debits; */
